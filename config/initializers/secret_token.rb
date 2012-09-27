2dca571d8220e9852b92b7139f5d4dc429411e99
Tessier <tessie_g@epitech.eu>
2012-09-27 15:59:56 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slider::Application.config.secret_token = '02ad8d90e38ea1526f0008b7abba35cc6ec685c52dd9bae1338e25c5efb03b68ad104d22ebba7cc0047d95c43281faf9f022bc2a1fe2f5757d3dc83668417fe8'
